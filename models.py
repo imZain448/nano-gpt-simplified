e666aac1fc22a96c1e9b74b8f91a35c41328ad4c
imZain448 <ahmadzain448@gmail.com>
2024-09-21 16:36:42 +0530

    attention model done
    - added code for multihead attention
    - improved the training loop

--- models.py
+++ models.py
@@ -31,12 +31,13 @@ class BigramLanguageModel(nn.Module):
 
 
 class SelfAttentionHead(nn.Module):
-    def __init__(self, head_size, n_embed, block_size):
+    def __init__(self, head_size, n_embed, block_size, dropout):
         super().__init__()
         self.key = nn.Linear(n_embed, head_size, bias=False)
         self.query = nn.Linear(n_embed, head_size, bias=False)
         self.value = nn.Linear(n_embed, head_size, bias=False)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -51,19 +52,99 @@ class SelfAttentionHead(nn.Module):
 
         weights = weights.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # B,T,T
         weights = F.softmax(weights, dim=-1)  # B,T,T
+        weights = self.dropout(weights)
         v = self.value(x)  # B,T,C
         out = weights @ v  # B,T,T x B,T,C --> B,T,C
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size, n_embed, block_size, dropout):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [
+                SelfAttentionHead(head_size, n_embed, block_size, dropout)
+                for _ in range(num_heads)
+            ]
+        )
+        self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, n_embed, dropout):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, n_embed),
+            nn.ReLU(),
+            nn.Linear(n_embed, n_embed),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class AttentionBlock(nn.Module):
+    def __init__(self, n_embed, num_heads, block_size, dropout):
+        super().__init__()
+        head_size = n_embed // num_heads
+        self.sa = MultiHeadAttention(num_heads, head_size, n_embed, block_size, dropout)
+        self.ffn = FeedForwardBlock(n_embed, dropout)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffn(self.ln2(x))
+        return x
+
+
 class SelfAttentionModel(nn.Module):
-    def __init__(self, vocab_size, n_embed, block_size, head_size):
+    def __init__(
+        self,
+        vocab_size,
+        n_embed,
+        block_size,
+        head_size,
+        num_heads=1,
+        dropout=0,
+        num_layers=0,
+        device="cpu",
+    ):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.sa_head = SelfAttentionHead(head_size, n_embed, block_size)
+        if num_layers > 0:
+            print(
+                "initializing the self attention model with multi head attention and multiple layers"
+            )
+            self.attention_block = nn.Sequential(
+                *[
+                    AttentionBlock(n_embed, num_heads, block_size, dropout)
+                    for _ in range(num_layers)
+                ]
+            )
+        elif num_heads > 1:  # use multi head attention
+            print("initializing the self attention model with multi head attention")
+            self.attention_block = MultiHeadAttention(
+                num_heads, head_size, n_embed, block_size, dropout
+            )
+        else:  # use single self attention
+            print("initializing the self attention model with single head attention")
+            self.attention_block = SelfAttentionHead(
+                head_size, n_embed, block_size, dropout
+            )
+
+        self.last_layer_norm = nn.LayerNorm(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
         self.block_size = block_size
+        self.device = device
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
@@ -71,10 +152,11 @@ class SelfAttentionModel(nn.Module):
             idx
         )  # output shape (B, T, n_embed) --
         position_embedding = self.position_embedding_table(
-            torch.arange(T)
+            torch.arange(T, device=self.device)
         )  # output shape (B, T, V)
         x = token_embedding * position_embedding  # output shape -- (B,T,n_embed)
-        x = self.sa_head(x)  # calling the self attention --> (B,T,n_embed)
+        x = self.attention_block(x)  # calling the self attention --> (B,T,n_embed)
+        x = self.last_layer_norm(x)
         logits = self.lm_head(x)  # output shape -- (B, T, V)
         if targets == None:
             return logits, None
--- train_attention.py
+++ train_attention.py
@@ -7,12 +7,22 @@ from models import SelfAttentionModel
 ## constants
 DATA_PATH = "./data/tiny_shakespeare.txt"
 VAIDATION_SPLIT = 0.1
-BLOCK_SIZE = 8
-BATCH_SIZE = 4
-EPOCHS = 10000
-N_EMBED = 32
+BLOCK_SIZE = 256
+BATCH_SIZE = 64
+TRAIN_STEPS = 4000
+EVAL_STEPS = 200
+N_EMBED = 384
 EVAL_INTERVALS = 500
-HEAD_SIZE = 32
+EPOCHS = TRAIN_STEPS // EVAL_INTERVALS
+NUM_HEADS = 6
+HEAD_SIZE = N_EMBED // NUM_HEADS
+NUM_LAYERS = 6
+DROPOUT = 0.2
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+CHECKPOINTS_PATH = "./ckpt/attention_checkpoints_v1_ckpt.pt"
+BEST_VAL_LOSS = float("inf")
+
+print(f"RNNING ON {DEVICE}")
 
 ## setting the seed
 torch.manual_seed(44)
@@ -31,10 +41,12 @@ print(f"size of train set -- {len(train_set)}")
 print(f"size of val set -- {len(val_set)}")
 
 
-def get_batch(data_set):
+def get_batch(split):
+    data_set = train_set if split == "train" else val_set
     ix = torch.randint(len(data_set) - BLOCK_SIZE, (BATCH_SIZE,))
     x = torch.stack([data_set[i : i + BLOCK_SIZE] for i in ix])
     y = torch.stack([data_set[i + 1 : i + BLOCK_SIZE + 1] for i in ix])
+    x, y = x.to(DEVICE), y.to(DEVICE)
     return x, y
 
 
@@ -46,21 +58,67 @@ print(xb)
 print("example targets batch")
 print(yb.shape)
 print(yb)
+model = SelfAttentionModel(
+    data_encoder.vocab_size,
+    N_EMBED,
+    BLOCK_SIZE,
+    HEAD_SIZE,
+    NUM_HEADS,
+    DROPOUT,
+    NUM_LAYERS,
+    DEVICE,
+)
 
-model = SelfAttentionModel(data_encoder.vocab_size, N_EMBED, BLOCK_SIZE, HEAD_SIZE)
-
+model.to(DEVICE)
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-for steps in tqdm(range(EPOCHS)):
-    xb, yb = get_batch(train_set)
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
 
-print(f"final loss after training -- {loss.item()}")
 
-print("TEXT GENERATED AFTER TRAINING THE BIGRAM MODEL")
+@torch.no_grad()
+def estimate_loss():
+    output = {}
+    model.eval()
+    for split in ["train", "val"]:
+        losses = torch.zeros(EVAL_STEPS)
+        for k in tqdm(range(EVAL_STEPS), colour="green"):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        output[split] = losses.mean()
+    model.train()
+    return output
 
-idx = torch.zeros((1, 1), dtype=torch.long)
+
+for epoch in range(EPOCHS):
+    print(f"Running EPOCH {epoch+1}/{EPOCHS}")
+    for steps in tqdm(range(EVAL_INTERVALS), colour="red"):
+        xb, yb = get_batch(train_set)
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+    print("Evaluating")
+    losses = estimate_loss()
+    print(f"Train loss : {losses['train']} | val loss : {losses['val']}")
+    if losses["val"] < BEST_VAL_LOSS:
+        print(
+            f"""validation loss improved {BEST_VAL_LOSS} to {losses['val']}
+                saving the model to {CHECKPOINTS_PATH}
+            """
+        )
+        checkpoint = {
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "epoch": epoch,
+            "best_val_loss": losses["val"],
+        }
+        BEST_VAL_LOSS = losses["val"]
+        torch.save(checkpoint, CHECKPOINTS_PATH)
+    print()
+
+# print(f"final loss after training -- {loss.item()}")
+
+print("TEXT GENERATED AFTER TRAINING THE ATTENTION MODEL")
+
+idx = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
 generated_text = data_encoder.decode(model.generate(idx, 1000)[0].tolist())
 print(generated_text)
